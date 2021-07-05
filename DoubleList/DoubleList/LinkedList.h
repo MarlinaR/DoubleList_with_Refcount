7414429f1747efcccb2af53de5a825277d0aef1b
MarlinaR <45965720+MarlinaR@users.noreply.github.com>
2021-07-05 11:27:25 +1000

    task 3_lock_node

---
+++ DoubleList/DoubleList/LinkedList.h
@@ -0,0 +1,496 @@
+#pragma once
+
+#include <functional>
+#include <utility>
+#include <type_traits>
+#include <queue>
+#include <limits>
+#include <numeric>
+#include <cmath>
+#include <memory>
+#include <stdexcept>
+#include <shared_mutex>
+#include <mutex>
+#include <iostream>
+#include <atomic>
+#include <cassert>
+
+using namespace std;
+
+template<typename T>
+class ListIterator;
+
+template<typename T>
+class LinkedList;
+
+template<typename T>
+class Node
+{
+    friend class ListIterator<T>;
+    friend class LinkedList<T>;
+
+private:
+    Node(LinkedList<T>* _list) : list(_list) { }
+    Node(T _value, LinkedList<T>* _list) : value(_value), ref_count(0), prev(this), next(this), list(_list) { }
+    Node(const Node<T>&) = delete;
+
+    ~Node() {
+        //cout << "Node destructor\n";
+    }
+
+    void operator=(const Node<T>&) = delete;
+
+    void release() {
+
+        if (--ref_count != 0) {
+            return;
+        }
+        list->DeleteNode(this);
+
+    }
+
+    void acquire() {
+        ref_count++;
+    }
+
+    T            value;
+    atomic<int>  ref_count = 0;
+    atomic<bool> deleted = false;
+    Node<T>*     prev;
+    Node<T>*     next;
+    shared_mutex m;
+    LinkedList<T>* list;
+};
+
+template<typename T>
+class ListIterator
+{
+public:
+    friend class LinkedList<T>;
+
+    ListIterator() noexcept = default;
+    ListIterator(const ListIterator& other) : node(other.node), list(other.node->list)
+    {
+        node->acquire();
+    }
+    ListIterator(Node<T>* _node) : node(_node), list(_node->list)
+    {
+        node->acquire();
+    }
+
+    ~ListIterator() {
+        if (!node) {
+            cout << "NODE IS NULL WHEN DELETING ITERATOR";
+            return;
+        }
+        list->ReleaseNode(node);
+    }
+
+    ListIterator& operator=(const  ListIterator& other) {
+        Node<T>* previousNode;
+        {
+            unique_lock<shared_mutex> lock1(node->m);
+            if (node == other.node) {
+                return *this;
+            }
+            shared_lock<shared_mutex> lock2(other.node->m);
+            previousNode = node;
+
+            node = other.node;
+            node->acquire();
+        }
+        list->ReleaseNode(previousNode);
+        //previousNode->release();
+        return *this;
+    }
+
+    T& operator*() const {
+        shared_lock<shared_mutex> lock(node->m);
+        if (node->deleted) throw (out_of_range("Invalid index"));
+        return node->value;
+    }
+
+    T* operator->() const {
+        shared_lock<shared_mutex> lock(node->m);
+        if (node->deleted) throw (out_of_range("Invalid index"));
+        return &(node->value);
+    }
+
+    // Prefix
+    ListIterator& operator++() {
+        unique_lock<shared_mutex> lock_first(node->m);
+        if (!node->next) throw (out_of_range("Invalid index"));
+
+        Node<T>* prevNode = node;
+        Node<T>* newNode = node->next;
+  
+        while (newNode->deleted && newNode->next) {
+            shared_lock<shared_mutex> lock(newNode->m);
+            newNode = newNode->next;
+        }
+        unique_lock<shared_mutex> lock(newNode->m);
+        newNode->acquire();
+        node = newNode;
+        list->ReleaseNode(prevNode);
+        //prevNode->release();
+
+        return *this;
+    }
+
+    // Postfix
+    ListIterator operator++(int) {
+        unique_lock<shared_mutex> lock_first(node->m);
+        if (!node->next) throw (out_of_range("Invalid index"));
+
+        Node<T>* prevNode = node;
+        Node<T>* newNode = node->next;
+
+        while (newNode->deleted && newNode->next) {
+            shared_lock<shared_mutex> lock(newNode->m);
+            newNode = newNode->next;
+        }
+
+        unique_lock<shared_mutex> lock(newNode->m);
+        newNode->acquire();
+        node = newNode;
+        list->ReleaseNode(prevNode);
+
+        return ListIterator(prevNode);
+    }
+
+    // Prefix
+    ListIterator& operator--() {
+        unique_lock<shared_mutex> lock_first(node->m);
+        if (!node->prev) throw out_of_range("Invalid index");
+
+        Node<T>* prevNode = node;
+        Node<T>* newNode = node->prev;
+
+        while (newNode->deleted && newNode->prev) {
+            shared_lock<shared_mutex> lock(newNode->m);
+            newNode = newNode->prev;
+
+
+        }
+        unique_lock<shared_mutex> lock(newNode->m);
+        newNode->acquire();
+        node = newNode;
+        list->ReleaseNode(prevNode);
+
+        return *this;
+    }
+
+    // Postfix
+    ListIterator operator--(int) {
+        unique_lock<shared_mutex> lock_first(node->m);
+        if (!node->prev) throw out_of_range("Invalid index");
+
+        Node<T>* prevNode = node;
+
+        Node<T>* newNode = node->prev;
+        while (newNode->deleted && newNode->prev) {
+            shared_lock<shared_mutex> lock(newNode->m);
+            newNode = newNode->prev;
+        }
+        unique_lock<shared_mutex> lock(newNode->m);
+        newNode->acquire();
+        node = newNode;
+        list->ReleaseNode(prevNode);
+
+        return ListIterator(node);
+    }
+
+    bool isEqual(const ListIterator<T>& other) const {
+        return node == other.node;
+    }
+
+    friend bool operator==(const ListIterator<T>& a, const ListIterator<T>& b) {
+        return a.isEqual(b);
+    }
+
+    friend bool operator!=(const ListIterator<T>& a, const ListIterator<T>& b) {
+        return !a.isEqual(b);
+    }
+
+    operator bool() {
+        shared_lock<shared_mutex> lock(node->m);
+        return node;
+    }
+
+    int debugRefCount()
+    {
+        shared_lock<shared_mutex> lock(node->m);
+        return node->ref_count;
+    }
+
+private:
+    Node<T>* node;
+    LinkedList<T>* list;
+};
+
+template<typename T>
+class LinkedList
+{
+public:
+    using iterator = ListIterator<T>;
+
+    friend class ListIterator<T>;
+
+    void ReleaseNode(Node<T>* node) {
+        --node->ref_count;
+        if (node->ref_count != 0) {
+            return;
+        }
+
+        queue<Node<T>*> nodesToDelete;
+        nodesToDelete.push(node);
+        while (!nodesToDelete.empty()) {
+            Node<T>* currentNode = nodesToDelete.front();
+            Node<T>* prev = currentNode->prev;
+            Node<T>* next = currentNode->next;
+            if (prev) {
+                --prev->ref_count;
+                if (prev->ref_count == 0) {
+                    nodesToDelete.push(prev);
+                }
+            }
+            if (next) {
+                --next->ref_count;
+                if (next->ref_count == 0) {
+                    nodesToDelete.push(next);
+                }
+            }
+            nodesToDelete.pop();
+            delete currentNode;
+        }
+    }
+
+    void DeleteNode(Node<T>* node) {
+        queue<Node<T>*> nodesToDelete;
+        nodesToDelete.push(node);
+
+        while (!nodesToDelete.empty()) {
+            Node<T>* currentNode = nodesToDelete.front();
+            Node<T>* prev = currentNode->prev;
+            Node<T>* next = currentNode->next;
+            if (prev) {
+                prev->ref_count--;
+                if (prev->ref_count == 0) {
+                    nodesToDelete.push(prev);
+                }
+            }
+            if (next) {
+                next->ref_count--;
+                if (next->ref_count == 0) {
+                    nodesToDelete.push(next);
+                }
+            }
+            nodesToDelete.pop();
+            delete (currentNode);
+        }
+    }
+
+    LinkedList() : head(nullptr), tail(nullptr), size(0) {
+        tail = new Node<T>(this);
+        head = new Node<T>(this);
+        tail->prev = head;
+        head->next = tail;
+
+        head->acquire();
+        head->acquire();
+        tail->acquire();
+        tail->acquire();
+    }
+
+    LinkedList(const LinkedList& other) = delete;
+    LinkedList(LinkedList&& x) = delete;
+    LinkedList(initializer_list<T> l) : LinkedList() {
+        iterator it(head);
+        for (auto item : l) {
+            push_back(item);
+        }
+    }
+
+    ~LinkedList() {
+
+        Node<T>* nextNode;
+        for (auto it = head; it != tail; it = nextNode) {
+            nextNode = it->next;
+            delete it;
+        }
+        delete tail;
+
+    }
+
+    LinkedList& operator=(const LinkedList& other) = delete;
+    LinkedList& operator=(LinkedList&& x) = delete;
+
+    // Returns iterator on next to erased node
+    iterator erase(iterator it) {
+
+        Node<T>* node = it.node;
+
+        if (node->deleted)
+            return nullptr;
+        if (node == head || node == tail) return it;
+
+        Node<T>* prev;
+        Node<T>* next;
+
+        for (bool retry = true; retry;) {
+            retry = false;
+            {
+                shared_lock<shared_mutex> currentLock(node->m);
+                prev = node->prev;
+                prev->acquire();
+                next = node->next;
+                next->acquire();
+                assert(prev && prev->ref_count);
+                assert(next && next->ref_count);
+            }
+            // RACES
+            { 
+                unique_lock<shared_mutex> prevLock(prev->m);
+                shared_lock<shared_mutex> currentLock(node->m);
+                unique_lock<shared_mutex> nextLock(next->m);
+
+                if (prev->next == node && next->prev == node) {
+
+                    prev->next = next;
+
+                    next->acquire();
+
+                    next->prev = prev;
+
+                    prev->acquire();
+
+
+                    node->deleted = true;
+                    ReleaseNode(node);
+                    ReleaseNode(node);
+                    //node->release();
+                    //node->release();
+                    --size;
+                }
+                else {
+                    retry = true;
+                }
+                ReleaseNode(prev);
+                ReleaseNode(next);
+                //prev->release();
+                //next->release();
+            }
+        }
+        return iterator(node->next);
+    }
+
+    void push_front(T value) {
+        iterator it(head);
+        insert_after(it, value);
+    }
+
+    void push_back(T value) {
+        Node<T>* node = tail->prev;
+        iterator it(node);
+        insert_after(it, value);
+    }
+
+    // Returns iterator on inserted node
+    iterator insert_after(iterator it, T value) {
+        Node<T>* prev = it.node;
+
+        // If called after begin() in empty list (where begin() == end())
+        if (prev == tail) {
+            prev = prev->prev;
+        }
+
+        if (prev == nullptr) return it;
+        prev->m.lock();
+
+        Node<T>* next = nullptr;
+
+        for (bool retry = true; retry;) {
+            retry = false;
+
+            next = prev->next;
+            next->m.lock();
+
+            if (next->prev != prev) {
+                retry = true;
+                next->m.unlock();
+            }
+        }
+
+        Node<T>* node = new Node<T>(value, this);
+
+        node->prev = prev;
+        node->next = next;
+
+        prev->next = node;
+        node->acquire();
+        next->prev = node;
+        node->acquire();
+
+        ++size;
+
+        prev->m.unlock();
+        next->m.unlock();
+
+        return iterator(node);
+    }
+
+
+    iterator begin() noexcept {
+        auto node = head->next;
+        unique_lock<shared_mutex> lock(node->m);
+        return iterator(node);
+    }
+
+    iterator end() noexcept {
+        auto node = tail;
+        unique_lock<shared_mutex> lock(node->m);
+        return iterator(node);
+    }
+
+    bool empty() noexcept {
+        return head->next == tail;
+    }
+
+
+    size_t Size() const
+    {
+        return size;
+    }
+
+    string debug() {
+        string output = "\n";
+
+        if (head->next == tail) return "\n[Empty list]\n";
+
+        Node<T>* current = head->next;
+        while (current != tail) {
+
+            if (!current || current->deleted)
+                throw new overflow_error("something gone wrong");
+
+            if (!current->next)
+                throw new overflow_error("something gone wrong 2");
+
+            output += "["
+                + to_string(current->value)
+                + ",ref:"
+                + to_string(current->ref_count)
+                + ",del:"
+                + to_string(current->deleted)
+                + "]\n";
+            current = current->next;
+        }
+        return output;
+    }
+
+private:
+    shared_mutex   m;
+    Node<T>*       head;
+    Node<T>*       tail;
+    atomic<size_t> size;
+};
--- DoubleList/DoubleList/Source.cpp
+++ DoubleList/DoubleList/Source.cpp
@@ -33,18 +33,60 @@
 #include <iostream>
 #include <atomic>
 #include <cstdlib>
+#include <ctime>
+#include <activation.h>
 
-#include "CLinkedList.hpp"  
-#include "Iterator.cpp"
+#include "LinkedList.h"
 
 #define CATCH_CONFIG_MAIN 
 #include "catch.hpp"
 
+using namespace std;
 
 
-TEST_CASE("LinkedList sample", "[CLinkedList]") {
-    SECTION("prefix/postfix ++/--") {
-        CLinkedList<int> list{ 1,2,3,4,5 };
+TEST_CASE("Setup") {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+TEST_CASE("Core mechanics tests", "[Base]") {
+
+
+    SECTION("Constructor, destructor, iterating, push") {
+        LinkedList<int> l;
+        l.push_back(1);
+        l.push_front(2);
+        l.push_back(3);
+
+        auto it = l.begin();
+
+        REQUIRE(*it == 2);
+        it++;
+        REQUIRE(*it == 1);
+        it++;
+        REQUIRE(*it == 3);
+        it--;
+        REQUIRE(*it == 1);
+        l.insert_after(it, 10);
+        it++;
+        REQUIRE(*it == 10);
+    }
+
+    SECTION("Initializer list constructor, destructor") {
+        LinkedList<int> l2{ 1,2,3,4,5 };
+        auto it = l2.begin();
+        REQUIRE(*it++ == 1);
+        REQUIRE(*it++ == 2);
+        REQUIRE(*it == 3);
+
+        LinkedList<bool>* l3 = new LinkedList<bool>{};
+        l3->push_back(true);
+        REQUIRE(*(l3->begin()) == true);
+        delete l3;
+    }
+
+    SECTION("Checking ref_count working") {
+        LinkedList<int> list{ 1,2,3,4,5 };
 
         auto it = list.begin();
         REQUIRE(*it == 1);
@@ -68,21 +110,24 @@ TEST_CASE("LinkedList sample", "[CLinkedList]") {
         ++it_3;
         REQUIRE(*it == 4);
 
-        list.insert(it_3,2);
-        list.insert(it_3, 3);
-        list.erase(it_3);
+        list.insert_after(it_3, 2);
+        list.insert_after(it_3, 3);
+        it_3 = list.erase(++it_3);
+        //it_3--;
+        it_3 = list.erase(it_3);
+
         it_3--;
-        list.erase(it_3);
+
         it_3 = it_2;
 
         it_3--;
         --it_2;
-        REQUIRE(*it_2 == 2);
-        REQUIRE(*it_3 == 2);
+        REQUIRE(*it_2 == 1);
+        REQUIRE(*it_3 == 1);
     }
-    
-    SECTION("operator==/!=") {
-        CLinkedList<int> list{ 1,2,3,4,5 };
+
+    SECTION("==, !=") {
+        LinkedList<int> list{ 1,2,3,4,5 };
 
         auto it = list.begin();
         auto it2 = list.begin();
@@ -90,18 +135,17 @@ TEST_CASE("LinkedList sample", "[CLinkedList]") {
 
         it2++;
         REQUIRE(it != it2);
-        REQUIRE(it.getRefCount() == 3);
-        REQUIRE(it2.getRefCount() == 3);
+        REQUIRE(it.debugRefCount() == 3);
+        REQUIRE(it2.debugRefCount() == 3);
         it++;
-        REQUIRE(it.getRefCount() == 4);
-
+        REQUIRE(it.debugRefCount() == 4);
     }
-    
+
     SECTION("push back/push front/insert") {
-        CLinkedList<int> list;
+        LinkedList<int> list;
 
         list.push_back(3);
-        REQUIRE( *--list.end() == 3);
+        REQUIRE(*--list.end() == 3);
 
         int a = 4;
         list.push_back(a);
@@ -115,18 +159,16 @@ TEST_CASE("LinkedList sample", "[CLinkedList]") {
         REQUIRE(*list.begin() == 1);
 
         auto it = list.begin();
-        for (int i = 1; i < 5; i++) {
+        for (int i = 1; i < 4; i++) {
             REQUIRE(*it == i);
             it++;
         }
 
-        it = list.insert(it,5);
-        REQUIRE(*it == 5);
-
+        it = list.insert_after(it, 5);
     }
-    
+
     SECTION("erase") {
-        CLinkedList<int> list{ 1,2,3 };
+        LinkedList<int> list{ 1,2,3 };
 
         auto it = list.begin();
         it = list.erase(it);
@@ -134,205 +176,154 @@ TEST_CASE("LinkedList sample", "[CLinkedList]") {
 
         it = list.erase(--list.end());
         REQUIRE(it == list.end());
-
     }
-    
-    SECTION("size") {
-        CLinkedList<int> list{ 1,2,3 };
 
-        REQUIRE(list.size() == 3);
+    SECTION("size") {
+        LinkedList<int> list{ 1,2,3 };
+
+        REQUIRE(list.Size() == 3);
 
         list.push_back(4);
-        REQUIRE(list.size() == 4);
+        REQUIRE(list.Size() == 4);
 
         list.push_front(1);
-        REQUIRE(list.size() == 5);
+        REQUIRE(list.Size() == 5);
+
+        auto it = list.begin();
+        list.erase(it);
+        it++;
+        REQUIRE(list.Size() == 4);
 
         list.erase(list.begin());
-        REQUIRE(list.size() == 4);
-
-        list.clear();
-        REQUIRE(list.size() == 0);
-
+        REQUIRE(list.Size() == 3);
     }
-    
+
     SECTION("->") {
-        CLinkedList<std::pair<int,int>> list;
-        list.push_back(std::make_pair(1, 2));
+        LinkedList<pair<int, int>> list;
+        list.push_back(make_pair(1, 2));
         auto it = list.begin();
         REQUIRE(it->first == 1);
     }
 
     SECTION("Is empty") {
-        CLinkedList<int> list;
-        REQUIRE(list.empty());
-
+        LinkedList<int> list;
+        REQUIRE(list.Size() == 0);
         list.push_back(4);
-        REQUIRE(!list.empty());
+        REQUIRE(list.Size() == 1);
     }
 
-    
-    SECTION("Threaded pushing, correct size of list, erase after threaded pushing") {
-        {
-            CLinkedList<int> list;
-            int threadsAmount = 100;
-            int pushes = 100;
-
-            std::vector<std::thread> vec;
-            for (int z = 0; z < threadsAmount; z++) {
-                vec.push_back(std::thread([&]() {
-                    auto it = list.begin();
-                    for (unsigned long i = 0l; i < pushes; i++) {
-                        list.push_back(1);
-                    }
-                }));
+    SECTION("Many pushes and erases") {
+        int tries = 1;
+        while (tries--) {
+            auto pushes = 10000;
+            //auto pushes = 1000000;
+            LinkedList<int>* list = new LinkedList<int>();
+            for (int i = 0; i < pushes; i++) {
+                list->push_back(i);
             }
-
-            for (int z = 0; z < threadsAmount; z++)
-            {
-                vec[z].join();
+            REQUIRE(list->Size() == pushes);
+            auto it = list->begin();
+            for (int j = 0; j < 1000000; j++) {
+                it = list->erase(it);
             }
-
-            REQUIRE(list.size() == (threadsAmount * pushes));
-
-            std::thread t2([&]() {
-                list.clear();
-            });
-
-            t2.join();
-
-            REQUIRE(list.size() == 0);
+            delete list;
         }
     }
-    
-    SECTION("Checks if read operations works in parallel") {
-        {
-            std::cout << "\n Begin test that require some time...\n";
-            CLinkedList<int> list{ 1, 2, 3, 4, 5};
-            int threadsAmount = 2;
-            int totalOperations = 5000000;
-
-            CHECK(totalOperations % threadsAmount == 0);
-
-            // Multi-threaded operations
-            auto startThreaded = std::chrono::high_resolution_clock::now();
-            std::vector<std::thread> vec;
-            for (int z = 0; z < threadsAmount; z++) {
-                vec.push_back(std::thread([&]() {
-                    for (unsigned long i = 0l; i < totalOperations / threadsAmount; i++) {
-                        list.size(); list.size(); list.size(); list.size(); list.size(); list.size();
-                        list.empty(); list.empty(); list.empty(); list.empty(); list.empty(); list.empty();
-                    }
-                }));
-            }
-            for (int z = 0; z < threadsAmount; z++)
-            {
-                vec[z].join();
-            }
-            auto endThreaded = std::chrono::high_resolution_clock::now();
-
-            // Single-thread operations
-            auto startSingle = std::chrono::high_resolution_clock::now();
-            for (unsigned long i = 0l; i < totalOperations; i++) {
-                list.size(); list.size(); list.size(); list.size(); list.size(); list.size(); list.size();
-                list.empty(); list.empty(); list.empty(); list.empty(); list.empty(); list.empty(); list.empty();
-            }
-            auto endSingle = std::chrono::high_resolution_clock::now();
-
-            // Check if multi-thread is faster
-            auto durationThreaded = std::chrono::duration_cast<std::chrono::microseconds>(endThreaded - startThreaded);
-            auto durationSingle = std::chrono::duration_cast<std::chrono::microseconds>(endSingle - startSingle);
-
-            CHECK(durationSingle > durationThreaded);
-        }
-    }
-    
-    SECTION("Erasing, iterating attack, checks end(), begin()") {
-        {
-            std::shared_mutex m;
-            std::condition_variable_any cv;
-
-            int threads = 10;
-            int elements = 1000;
-
-            CLinkedList<int> list;
-            std::vector<int> savedValues;
-            std::atomic<bool> dataReady{ false };
-            std::atomic<int> consumed{ 0 };
-            std::vector<int> wrongValues;
-
-            std::vector<std::thread> checkers;
-            for (int i = 0; i < threads; i++)
-            {
-                checkers.push_back(std::thread([&]() {
-                    std::cout << "Checker waiting\n";
-                    std::shared_lock<std::shared_mutex> lck(m);
-                    cv.wait(lck, [&] { return dataReady.load(); });
-
-                    std::cout << "Running checks [ERASE, it++]\n";
-                    auto it = list.begin();
-                    for (int i = 0; i < elements / (threads * 3); i++)
-                    {
-                        it++;
-                        it = list.erase(it);
-                    }
-
-                    dataReady = false;
-                    consumed++;
-                    std::cout << "Checks complete\n";
-                    cv.notify_all();
-                    cv.wait(lck, [&]() {return dataReady == true; });
-
-                    std::cout << "Running checks [END, it++, GET, BEGIN]\n";
-                    it = list.begin();
-                    while (it != list.end())
-                    {
-                        it++;
-                    }
-                    auto val = it.get() + it.get() % 10;
-                    savedValues.push_back(val);
-
-                    dataReady = false;
-                    consumed++;
-                    std::cout << "Checks complete\n";
-                    cv.notify_all();
-                }));
-            }
-
-            std::thread producer([&]() {
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-                std::unique_lock<std::shared_mutex> lck(m);
-
-                for (int i = 0; i < elements; i++) {
-                    list.push_front(rand());
-                }
-                dataReady = true;
-
-                std::cout << "Data prepared\n";
-                cv.notify_all();
-                cv.wait(lck, [&]() { return consumed.load() == threads; });
-
-                std::cout << "Data preparing\n";
-                list.push_front(rand());
-                dataReady = true;
-                consumed = 0;
-
-
-                std::cout << "Data prepared\n";
-                cv.notify_all();
-                cv.wait(lck, [&]() { return consumed.load() == threads; });
-
-                std::cout << "Checking results\n";
-                auto correctValue = savedValues[0];
-                std::copy_if(savedValues.begin(), savedValues.end(), std::back_inserter(wrongValues), [&](int i) {return i != correctValue; });
-            });
-
-            for (auto& th : checkers)
-                th.join();
-            producer.join();
-
-            REQUIRE(wrongValues.size() == 0);
-        }
-    }
-    
 }
+
+void do_join(thread& t) { t.join(); }
+void join_all(vector<thread>& v) { for_each(v.begin(), v.end(), do_join); }
+
+auto pusherFunction = [](LinkedList<int>* list, ListIterator<int> it, const int& numberOfPushes, const int& value)
+{
+    for (auto i = 0; i < numberOfPushes; i++) {
+        it = list->insert_after(it, value);
+    }
+    //printf("Pusher %i ended with %i pushes\n", value, numberOfPushes);
+};
+
+auto deleterFunction = [](LinkedList<int>* list, ListIterator<int> it, const int& numberOfErases)
+{
+    for (auto i = 0; i < numberOfErases; i++) {
+        it = list->erase(it);
+    }
+    //printf("Eraser ended with %i erases\n", numberOfErases);
+};
+
+void ThreadedTest1(const int threads, int tries, const int totalPushes) {
+    const int pushesPerThread = totalPushes / threads;
+    while (tries--) {
+
+        LinkedList<int>* list = new LinkedList<int>();
+        vector<ListIterator<int>> iterators;
+        ListIterator<int>* it = new ListIterator<int>(list->begin());
+        for (int i = 0; i < threads; i++) {
+            *it = list->insert_after(*it, i);
+            iterators.push_back(*it);
+            *it = list->insert_after(*it, i);
+        }
+        delete it;
+
+        REQUIRE(list->Size() == threads * 2);
+
+        // Pushers
+        {
+            vector<thread> pushers;
+            pushers.reserve(threads);
+            for (int i = 0; i < threads; i++) {
+                pushers.push_back(
+                    thread(pusherFunction, list, iterators[i], pushesPerThread, i)
+                );
+            }
+            join_all(pushers);
+        }
+
+        REQUIRE(list->Size() == threads * (pushesPerThread + 2));
+
+        {
+            vector<thread> deleters;
+            deleters.reserve(threads);
+            for (int i = 0; i < threads; i++) {
+                deleters.push_back(
+                    thread(deleterFunction, list, iterators[i], pushesPerThread)
+                );
+            }
+            join_all(deleters);
+        }
+
+
+
+        REQUIRE(list->Size() == threads * 2);
+        delete list;
+    }
+}
+
+
+TEST_CASE("Threaded tests", "[threads]") {
+    auto hardwareThreadsAmount = thread::hardware_concurrency();
+
+    SECTION("Threaded pushing, correct size of list, erase after threaded pushing") {
+        auto repeatEveryTest = 5;
+        auto totalPushes = 100000;
+        int threadAmount = 1;
+        //int threadAmount = 4;
+        int pushesPerThread;
+
+        while (threadAmount <= hardwareThreadsAmount) {
+            //std::cout << "Multithreaded test, " << threadAmount << " threads\n\n";
+            pushesPerThread = totalPushes / threadAmount;
+            clock_t start = clock();
+
+            ThreadedTest1(threadAmount, repeatEveryTest, totalPushes);
+            cout << "\nThreaded test. "
+                << threadAmount << " threads, "
+                << pushesPerThread << " elements per thread, "
+                << totalPushes << "total elements,"
+                << float(clock() - start) / CLOCKS_PER_SEC * 1000 << " msec." << endl;
+
+            threadAmount *= 2;
+        }
+    }
+}
+
+
